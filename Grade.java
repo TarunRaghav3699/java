293703ef247aa56b489655eee9dcb23eeced5790
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-09-20 17:14:05 +0000

    added 2 file

--- Grade.java
+++ Grade.java
@@ -1,33 +1,33 @@
-import java.util.Scanner;
-class Grade{
-    public void grade(){
-        int marks = percent;
-        if (marks > 75){
-            System.out.println("Grade A, Excellent");
-        }else if (marks <75 && marks >= 60){
-            System.out.println("Grade B, Very Good");
-        }else if (marks < 60 && marks >= 45){
-            System.out.println("Grade C, Good");
-        }else if (marks < 45 && marks >= 33){
-            System.out.println("Grade D, Pass");
-        }else if( marks <33) {
-            System.out.println("Grade F, Fail");
-        }
-    }
-    public static void main(String[] args){
-        Grade myGrade = new Grade();
-        Scanner myObj = new Scanner(System.in);
-        int marks = 0;
-        for (int i = 0; i <5; i++){
-            System.out.print("Enter your marks: ");
-            int mark = myObj.nextInt();
-            marks += mark;
-        }
-        int percent = marks/5;
-    System.out.print("Total marks is "+ marks +"\n");
-    System.out.print("Total percent is "+ percent +"%\n");
-    String a = myGrade.grade(percent);
-    System.out.println(a);
-    myObj.close();
-    }
-}
+// import java.util.Scanner;
+// class Grade{
+//     public void grade(){
+//         int marks = percent;
+//         if (marks > 75){
+//             System.out.println("Grade A, Excellent");
+//         }else if (marks <75 && marks >= 60){
+//             System.out.println("Grade B, Very Good");
+//         }else if (marks < 60 && marks >= 45){
+//             System.out.println("Grade C, Good");
+//         }else if (marks < 45 && marks >= 33){
+//             System.out.println("Grade D, Pass");
+//         }else if( marks <33) {
+//             System.out.println("Grade F, Fail");
+//         }
+//     }
+//     public static void main(String[] args){
+//         Grade myGrade = new Grade();
+//         Scanner myObj = new Scanner(System.in);
+//         int marks = 0;
+//         for (int i = 0; i <5; i++){
+//             System.out.print("Enter your marks: ");
+//             int mark = myObj.nextInt();
+//             marks += mark;
+//         }
+//         int percent = marks/5;
+//     System.out.print("Total marks is "+ marks +"\n");
+//     System.out.print("Total percent is "+ percent +"%\n");
+//     String a = myGrade.grade(percent);
+//     System.out.println(a);
+//     myObj.close();
+//     }
+// }
---
+++ Practice.java
@@ -0,0 +1,13 @@
+class Practice{
+    static int factorial(int num){
+        if (num == 1) {
+            return 1;
+        }else{
+            return num * factorial(num-1);
+        }
+    }
+    public static void main(String[] args){
+        int value = factorial(5);
+        System.out.println(value);
+    }
+}
