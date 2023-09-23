0503993d8c38a63c9a1b5e645209af4ee0cc72f0
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-09-23 01:50:41 +0000

    make a code for Gcd and Lcm

---
+++ Gcd.java
@@ -0,0 +1,29 @@
+// read this code from a book Discrete Mathematics by CL Lie and DP Mohapatra
+// Code to find GCD (Greatest Common Divisor) in other words GCD is equal to HCF(Highest common factor)
+// if anyone have any doubt the refer to the txt file doubt
+
+import java.util.Scanner;
+class Gcd{
+    // Definition of the GCD Function
+    public int GCD(int n, int m){
+        if ((n>=m) && ((n%m)==0)){
+            return (m);
+        }else {
+            return GCD(m,(n%m));
+        }
+    }
+    public static void main(String[] args){
+        Gcd newList = new Gcd();
+        Scanner myObj = new Scanner(System.in);
+        int n, m;
+        int result;
+        System.out.print("Input the first integer number: ");
+        n = myObj.nextInt();
+        System.out.print("Input the second integer number: ");
+        m = myObj.nextInt();
+
+        result = newList.GCD(n,m);
+        System.out.printf("Greatest Common Divisor of : %d and %d is = %d",n,m,result,"\n");
+        myObj.close();
+    }
+}
---
+++ Lcm.java
@@ -0,0 +1,34 @@
+// In this i am Calculating the LCM(least common multiple)
+// if anyone have any doubt the refer to the txt file doubt
+
+import java.util.Scanner;
+class Lcm {
+    public int gcd(int m, int n){
+        int ans;
+        if (m<n){
+            ans = gcd(n,m);
+        }else if(n==0){
+            ans = m;
+        }else {
+            ans = gcd(n,m%n);
+        }
+        return (ans);
+    }
+    public static void main(String[] args){
+        int m, n, lcm1, hcf1;
+        Lcm newLine = new Lcm();
+        Scanner myObj = new Scanner(System.in);
+        char a = 'y';
+        System.out.println("Calculating LCM of two positive numbers from their GCD \n");
+        do{
+            System.out.println("Enter the two numbers: ");
+            m = myObj.nextInt();
+            n = myObj.nextInt();
+            hcf1 = newLine.gcd(m,n);
+            lcm1 = (m*n)/hcf1;
+            System.out.printf("\n LCM of %d and %d is: %d\n Do you want to find LCM of two more Numbers?\ny/n\n",m,n,lcm1);
+            a = myObj.next().charAt(0);
+
+        }while(a == 'y');
+    }
+}
