31c62bb8d5bbcae7964a8c4327efde9832271620
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-10-09 16:16:16 +0000

    .

--- Armstrong.java
+++ Armstrong.java
@@ -1,16 +1,48 @@
-class Armstrong{
-    public static void main(String[] args){
-        int num = 15051,r,sum = 0;
-        int num2 = num;
-        while(num>0){
-            r = num%10;
-            sum = (sum*10)+r;
-            num/=10;
+import java.util.Scanner;
+
+public class Armstrong {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        if (isArmstrong(number)) {
+            System.out.println(number + " is an Armstrong number.");
+        } else {
+            System.out.println(number + " is not an Armstrong number.");
         }
-        if(sum==num2){
-            System.out.println("This is an armstrong number "+sum+" = "+num2);
-        }else {
-            System.out.println("This is not an armstrong number "+sum + " != "+num2);
+    }
+
+    public static int counting(int num) {
+        // int r, sum = 0;
+        int count = 0;
+
+        while (num > 0) {
+            // r = num % 10;
+            // sum = sum + r;
+            if (num % 10 > (0)) {
+                count = count + 1;
+            }
+            num /= 10;
         }
+        System.out.println("count "+count+Math.valueOf(num));
+        return count;
+    }
+
+    public static boolean isArmstrong(int num) {
+        int originalNum, remainder, result = 0;
+        originalNum = num;
+
+        // Calculate the sum of the cubes of each digit
+        while (originalNum != 0) {
+            remainder = originalNum % 10;
+            result += Math.pow(remainder, counting(originalNum));
+            originalNum /= 10;
+        }
+
+        // Check if the result is equal to the original number
+        return result == num;
     }
 }
