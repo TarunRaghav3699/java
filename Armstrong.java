0eed0fa72d0aa31a9b97fb7904a70d06bf456dce
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-10-10 19:59:20 +0530

    major changes

--- Armstrong.java
+++ Armstrong.java
@@ -1,48 +1,32 @@
 import java.util.Scanner;
 
-public class Armstrong {
-
+public class ArmstrongNumber {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         System.out.print("Enter a number: ");
-        int number = scanner.nextInt();
-        scanner.close();
-
-        if (isArmstrong(number)) {
-            System.out.println(number + " is an Armstrong number.");
-        } else {
-            System.out.println(number + " is not an Armstrong number.");
-        }
-    }
-
-    public static int counting(int num) {
-        // int r, sum = 0;
-        int count = 0;
-
-        while (num > 0) {
-            // r = num % 10;
-            // sum = sum + r;
-            if (num % 10 > (0)) {
-                count = count + 1;
-            }
-            num /= 10;
-        }
-        System.out.println("count "+count+Math.valueOf(num));
-        return count;
-    }
-
-    public static boolean isArmstrong(int num) {
+        int num = sc.nextInt();
         int originalNum, remainder, result = 0;
+        int n = 0;
+
         originalNum = num;
 
-        // Calculate the sum of the cubes of each digit
+        // Count the number of digits in the number
+        while (originalNum != 0) {
+            originalNum /= 10;
+            n++;
+        }
+
+        originalNum = num;
+
         while (originalNum != 0) {
             remainder = originalNum % 10;
-            result += Math.pow(remainder, counting(originalNum));
+            result += Math.pow(remainder, n);
             originalNum /= 10;
         }
 
-        // Check if the result is equal to the original number
-        return result == num;
+        if (result == num)
+            System.out.println(num + " is an Armstrong number.");
+        else
+            System.out.println(num + " is not an Armstrong number.");
     }
 }
