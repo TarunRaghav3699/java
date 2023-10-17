d7197d48621ebda90fc9efaa9bab6f5c5b4758f2
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-10-17 07:00:31 +0000

    include Scanner class in factorial

--- Factorial.java
+++ Factorial.java
@@ -1,6 +1,10 @@
+import java.util.Scanner;
+
 public class Factorial {
     public static void main(String[] args) {
-        int number = 5; 
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter the number: ");
+        int number = myObj.nextInt(); 
         long factorial = 1;
 
         for (int i = 1; i <= number; i++) {
@@ -8,5 +12,6 @@ public class Factorial {
         }
 
         System.out.println("Factorial of " + number + " is " + factorial);
+        myObj.close();
     }
 }
--- Practice3.java
+++ Practice3.java
@@ -1,4 +1,4 @@
-// Example of Constructor Overloading
+import java.util.Scanner;
 
 class Shape {
     private double length;
@@ -6,12 +6,13 @@ class Shape {
     private double base;
     private double height;
 
-    // Constructor overloading
+    // Constructor for a rectangle
     public Shape(double l, double b) {
         this.length = l;
         this.breadth = b;
     }
 
+    // Constructor for a triangle
     public Shape(double b, double h) {
         this.base = b;
         this.height = h;
@@ -33,10 +34,31 @@ class Shape {
 
 public class Practice3 {
     public static void main(String[] args) {
-        Shape rectangle = new Shape(5.0, 4.0);
+        Scanner scanner = new Scanner(System.in);
+        double l, b, base, height;
+
+        System.out.println("Enter dimensions for the rectangle (length breadth):");
+        l = scanner.nextDouble();
+        b = scanner.nextDouble();
+
+        System.out.println("Enter dimensions for the triangle (base height):");
+        base = scanner.nextDouble();
+        height = scanner.nextDouble();
+
+        Shape rectangle = new Shape(l, b);
+        Shape triangle = new Shape(base, height);
+
         System.out.println("Area of rectangle: " + rectangle.area());
 
-        Shape triangle = new Shape(3.0, 6.0);
-        System.out.println("Area of triangle: " + triangle.area("triangle"));
+        System.out.println("Do you want to calculate the area of a triangle (yes/no)?");
+        String choice = scanner.next();
+
+        if (choice.equalsIgnoreCase("yes")) {
+            System.out.println("Area of triangle: " + triangle.area("triangle"));
+        } else {
+            System.out.println("No area of triangle calculated.");
+        }
+
+        scanner.close(); // Close the scanner when done
     }
 }
