d18f683803d54c00ff175dfba8034c75c564189e
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-10-26 06:30:29 +0000

    make an array sorting

--- ArraySorting.java
+++ ArraySorting.java
@@ -1,1 +1,21 @@
-String[] arr= {"bombay", "delhi", "kolkata", "mumbai"};
+class ArraySorting{
+    public static void main(String[] args){
+        String[] arr= {"mumbai","bombay", "delhi", "kolkata"};
+        // System.out.println(arr.length());
+        int n = 4;
+        String temp;
+        for (int i = 0; i<n; i++){
+            for (int j = i+1; j<n; j++){
+                if (arr[i].compareTo(arr[j])>0){
+                    // swaping
+                    temp = arr[i];
+                    arr[i]= arr[j];
+                    arr[j]= temp;
+                }
+            }
+        }
+        for (int i= 0; i<arr.length; i++ ){
+        System.out.println(arr[i]);
+        }
+    }
+}
