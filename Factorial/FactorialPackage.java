742faec20f9e08d980601dff5ed9b6202787fe95
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-07 05:06:56 +0000

    .

--- Factorial/FactorialPackage.java
+++ Factorial/FactorialPackage.java
@@ -1,18 +1,13 @@
 package Factorial;
 import java.util.Scanner;
 public class FactorialPackage{
-    // public int data;
-
     public static void main(String[] args){
         Scanner myScanner = new Scanner(System.in);
         int num = myScanner.nextInt();
         factorial(num);
-        
-        // System.out.println(fib);
         myScanner.close();
     }
        public static int factorial(int num){
-        // FibonacciPackage myObj = new FibonacciPackage();
         int fact = 1;
         for (int i= 1; i<=num; i++){
             fact*=i;
