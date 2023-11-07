0d82b3748532d8f6727f04379ed6083e00de1d58
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-07 05:04:15 +0000

    .

--- FactorialPackage1.java
+++ FactorialPackage1.java
@@ -1,9 +1,13 @@
 import Factorial.FactorialPackage;
+import java.util.Scanner;
 
 public class FactorialPackage1 {
     public static void main(String[] args){
-        int number = 5;
+        Scanner myObj = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int number = myObj.nextInt();
         int num = FactorialPackage.factorial(number);
         System.out.println("factorial of " + number + " is " +num);
+        myObj.close();
     }
 }
