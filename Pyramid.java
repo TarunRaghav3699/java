c68840883250da595b85b9eafc59316298c7b038
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-14 05:43:04 +0000

    made a star pyramid

---
+++ Pyramid.java
@@ -0,0 +1,27 @@
+class Pyramid {
+    public static void main(String[] args) {
+        int n = 5;
+        // outer loop (rows)
+        for (int i = n; i >= 0; i--) {
+            // inner loop (columns)
+            for (int j = 0; j <= n; j++) {
+                if ( j == n || i == j || i<=j) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+// output will be:-
+
+//   1 2 3 4 5 6
+// 1 + + + + + * 
+// 2 + + + + * * 
+// 3 + + + * * * 
+// 4 + + * * * * 
+// 5 + * * * * * 
+// 6 * * * * * * 
