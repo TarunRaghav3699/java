a659442428b3a03a3928b7889b5cf85aadd3b60d
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-18 04:20:13 +0000

    0-1 triangle

---
+++ ZeroOneTriangle.java
@@ -0,0 +1,26 @@
+// not for exam
+
+public class ZeroOneTriangle {
+    public static void main(String[] args){
+        for (int i = 0; i <= 5; i++){
+            for(int j = 0; j<i; j++){
+                if ((i+j)%2==0){
+                System.out.print("0 ");
+                }else{
+                    System.out.print("1 ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+// output will be:-
+
+//    0 1 2 3 4
+// 1. 1 + + + +
+// 2. 0 1 + + +
+// 3. 1 0 1 + +
+// 4. 0 1 0 1 +
+// 5. 1 0 1 0 1 
