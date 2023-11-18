d1a6aadfe3653ef4784f67093ae1425f79be943e
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-18 04:21:05 +0000

    0-1 Traingle

--- ZeroOneTriangle.java
+++ ZeroOneTriangle.java
@@ -1,12 +1,12 @@
 // not for exam
 
 public class ZeroOneTriangle {
-    public static void main(String[] args){
-        for (int i = 0; i <= 5; i++){
-            for(int j = 0; j<i; j++){
-                if ((i+j)%2==0){
-                System.out.print("0 ");
-                }else{
+    public static void main(String[] args) {
+        for (int i = 0; i <= 5; i++) {
+            for (int j = 0; j < i; j++) {
+                if ((i + j) % 2 == 0) {
+                    System.out.print("0 ");
+                } else {
                     System.out.print("1 ");
                 }
             }
@@ -15,12 +15,11 @@ public class ZeroOneTriangle {
     }
 }
 
-
 // output will be:-
 
-//    0 1 2 3 4
+// 0 1 2 3 4
 // 1. 1 + + + +
 // 2. 0 1 + + +
 // 3. 1 0 1 + +
 // 4. 0 1 0 1 +
-// 5. 1 0 1 0 1 
+// 5. 1 0 1 0 1
