f0953d8f74197168e9559b72a3b12042a7760e7b
Tarun_Raghav3699 <124661867+TarunRaghav3699@users.noreply.github.com>
2023-11-30 10:00:11 +0000

    .

---
+++ StudentResult.java
@@ -0,0 +1,56 @@
+interface Sports {
+    int sportsWeight = 10;
+    void display();
+}
+
+class Student {
+    String name, rollNumber, course, section;
+
+    void display() {
+        System.out.println("Student Info: \nName: " + name + "\nRoll Number: " + rollNumber +
+                "\nCourse: " + course + "\nSection: " + section);
+    }
+}
+
+class Test extends Student {
+    String subject1, subject2;
+    int marks;
+
+    void display() {
+        super.display();
+        System.out.println("Test Info: \nSubject 1: " + subject1 + "\nSubject 2: " + subject2 +
+                "\nMarks: " + marks);
+    }
+}
+
+class Result extends Test implements Sports {
+    void compare() {
+        int average = (Integer.parseInt(subject1) + Integer.parseInt(subject2) + sportsWeight) / 3;
+        System.out.println("Average: " + average);
+    }
+
+    public void display() {
+        super.display();
+        System.out.println("Sports Info: \nSports Weight: " + sportsWeight);
+    }
+}
+
+public class StudentResult implements Sports {
+    public static void main(String[] args) {
+        Result result = new Result();
+        result.name = "Tarun";
+        result.rollNumber = "106";
+        result.course = "B.Tech";
+        result.section = "B";
+        result.subject1 = "75";
+        result.subject2 = "80";
+        result.marks = 85;
+
+        result.display();
+        result.compare();
+    }
+
+    public void display() {
+        System.out.println("Student Result Info: \nSports Weight: " + sportsWeight);
+    }
+}
